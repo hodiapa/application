1d7e8ce503f5b988f8a397d579f89c6452475353
Bengi Mizrahi <bengi.mizrahi@argela.com.tr>
2012-06-19 13:12:23 +0300

    Give an example usage of Thread

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "system/state.h"
 #include "system/message.h"
+#include "system/thread.h"
 
 #include <iostream>
 #include <string>
@@ -29,12 +30,26 @@ EXTENDED_STATE(StateXX, StateX)
     void handleC(Message *m) {std::cout << "handleC()" << std::endl; }
 STATE_END
 
+class MyThread : public Thread {
+public:
+    virtual void run() {
+        for (int i = 0; i < 1000; i++) {
+            std::cout << i << std::endl;
+        }
+    }
+};
+
 int main(int argc, char **argv) {
     StateXX state;
     Message *m = new MessageA();
     state.handle(m);
     m = new MessageB();
     state.handle(m);
+
+    MyThread t;
+    t.start();
+    t.join();
+
     return 0;
 }
 
